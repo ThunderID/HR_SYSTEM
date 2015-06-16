5c5ecd1329e22d6681a7c75ade5b651b3b0c30bc
chelsymooy <chelsymooy1108@gmail.com>
2015-06-16 11:23:14 +0700

    Fixed crashed branch

--- resources/views/pages/branch/index.blade.php
+++ resources/views/pages/branch/index.blade.php
@@ -1,5 +1,5 @@
 @section('nav_topbar')
-	@include('widgets.common.nav_topbar', ['breadcrumb' => [['name' => $data['name'], 'route' => route('hr.organisations.show', [$data['id'], 'org_id' => $data['id']])]]])
+	@include('widgets.common.nav_topbar', ['breadcrumb' => [['name' => $data['name'], 'route' => route('hr.organisations.show', [$data['id'], 'org_id' => $data['id']]) ], ['name' => 'Cabang', 'route' => route('hr.branches.index', ['org_id' => $data['id']]) ]]])
 @stop
 
 @section('nav_sidebar')
@@ -8,15 +8,13 @@
 		'widget_title'			=> 'Structure',		
 		'widget_title_class'	=> 'text-uppercase ml-10 mt-20',
 		'widget_body_class'		=> '',
-		'widget_options'	=> ['widget_title'		=> 'Pilih Organisasi :',								
-								'organisation_id'	=> 1,
-								'identifier'		=> 1,
-								'document_id'		=> 1,
-								'search'			=> [],
-								'sort'				=> [],
-								'page'				=> 1,
-								'per_page'			=> 12,
-								]
+		'widget_options'		=> 	[
+										'identifier'		=> 1,
+										'search'			=> [],
+										'sort'				=> [],
+										'page'				=> 1,
+										'per_page'			=> 12,
+									]
 	])
 @overwrite
 
@@ -30,25 +28,16 @@
 @overwrite
 
 @section('content_body')	
-	@include('widgets.common.show_info', [
-		'widget_template'		=> 'plain',
-		'widget_title'			=> $data['name'],
-		'widget_title_class'	=> '',
-		'widget_body_class'		=> '',
-		'widget_info'			=> 'Total Cabang',
-		'widget_info_class'		=> 'mb-10',
-		'widget_options'		=> ['total'	=> count($branches)]		
-	])
-	
 	@include('widgets.branch.table', [
-		'widget_template'		=> 'plain_no_title',
-		'widget_options'		=> ['form_url' 			=> null,
-									'organisation_id'	=> $data['id'],
-									'identifier'		=> 1,
-									'search'			=> ['defaultcontact' => true],
-									'sort'				=> [],
-									'page'				=> 1,
-									'per_page'			=> 12
+		'widget_template'		=> 'panel',
+		'widget_options'		=> [
+										'form_url' 			=> null,
+										'organisation_id'	=> $data['id'],
+										'identifier'		=> 1,
+										'search'			=> ['defaultcontact' => true],
+										'sort'				=> [],
+										'page'				=> 1,
+										'per_page'			=> 12
 									]
 	])
 
