a3705c5c65349e2a42ad0d9c98893bc9dfe9417e
uchuy77 <agil.mahendra@gmail.com>
2015-06-24 13:49:55 +0700

    modified select2, select2 tags

--- resources/views/widgets/chart/select.blade.php
+++ resources/views/widgets/chart/select.blade.php
@@ -1,4 +1,4 @@
-<select name="chart_id" class="form-control">
+<select name="chart_id" class="form-control select2">
 	@foreach($ChartComposer['widget_data']['chartlist']['chart'] as $key => $value)
 		<option value="{{$value['id']}}" @if($value['id']==$ChartComposer['widget_data']['chartlist']['chart_id']) selected @endif>{{$value['name']}} departemen {{$value['tag']}} cabang {{$value['branch']['name']}} </option>
 	@endforeach
