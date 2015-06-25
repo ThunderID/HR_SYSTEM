3bc25947aab5f69a1f4524b803127b919c2dd775
uchuy77 <agil.mahendra@gmail.com>
2015-06-25 16:09:41 +0700

    select to add tabindex

--- resources/views/widgets/calendar/select.blade.php
+++ resources/views/widgets/calendar/select.blade.php
@@ -1,4 +1,4 @@
-<select name="calendar_id" class="form-control select2">
+<select name="calendar_id" class="form-control select2" tabindex="{{ $CalendarComposer['widget_data']['calendarlist']['tabindex'] }}">
 	@foreach($CalendarComposer['widget_data']['calendarlist']['calendar'] as $key => $value)
 		<option value="{{$value['id']}}" @if($value['id']==$CalendarComposer['widget_data']['calendarlist']['calendar_id']) selected @endif>{{$value['name']}}</option>
 	@endforeach
--- resources/views/widgets/organisation/select.blade.php
+++ resources/views/widgets/organisation/select.blade.php
@@ -5,25 +5,19 @@
 @overwrite
 
 @section('widget_body')
-	{!! Form::open(['url' => $OrganisationComposer['widget_data']['organisationlist']['form_url'], 'class' => 'form-horizontal', 'method' => 'get']) !!}	
-		<div class="form-group">
-			<div class="col-md-3">
-				<label class="control-label">Akses Perusahaan</label>
-			</div>	
-			<div class="col-md-9">
-				<select name="org_id" id="org" class="select2 form-control" style="width:100%">
-					@if(isset($OrganisationComposer['widget_data']['organisationlist']['organisation']))
-						@foreach($OrganisationComposer['widget_data']['organisationlist']['organisation'] as $key => $value)
-							<option value="{{ $value['id'] }}">{{ $value['name'] }}</option>
-						@endforeach
-					@endif
-				</select>
-			</div>
+	{!! Form::open(['url' => $OrganisationComposer['widget_data']['organisationlist']['form_url'], 'class' => 'form', 'method' => 'get']) !!}	
+		<div class="form-group">			
+			<label class="control-label">Akses Perusahaan</label>			
+			<select name="org_id" id="org" class="select2 form-control" style="width:100%" tabindex="1">
+				@if(isset($OrganisationComposer['widget_data']['organisationlist']['organisation']))
+					@foreach($OrganisationComposer['widget_data']['organisationlist']['organisation'] as $key => $value)
+						<option value="{{ $value['id'] }}">{{ $value['name'] }}</option>
+					@endforeach
+				@endif
+			</select>			
 		</div>
-		<div class="form-group">
-			<div class="col-md-12 text-right">
-				<input type="submit" class="btn btn-primary" value="Pilih">
-			</div>
+		<div class="form-group text-right">			
+			<input type="submit" class="btn btn-primary" value="Pilih" tabindex="2">
 		</div>
 	{!! Form::close() !!}
 @overwrite	
