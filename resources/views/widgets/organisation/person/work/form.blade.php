e606972839c92e2b250c809feebee720af450898
chelsymooy <chelsymooy1108@gmail.com>
2015-06-28 13:50:28 +0700

    Fixed #20

--- resources/views/widgets/organisation/person/work/form.blade.php
+++ resources/views/widgets/organisation/person/work/form.blade.php
@@ -2,7 +2,7 @@
 
 @if (!$widget_error_count)
 	@section('widget_title')
-	<h1> Karir </h1>
+		<h1> {{ is_null($id) ? 'Tambah Karir ' : 'Ubah Karir '}} "{{$person['name']}}" </h1> 
 	@overwrite
 
 	@section('widget_body')
@@ -13,7 +13,7 @@
 					<label class="control-label">Posisi</label>
 				</div>	
 				<div class="col-md-10">
-					@include('widgets.chart.select', [
+					@include('widgets.organisation.branch.chart.select', [
 						'widget_options'		=> 	[
 														'chartlist'			=>
 														[
@@ -23,6 +23,7 @@
 															'page'				=> 1,
 															'per_page'			=> 100,
 															'chart_id'			=> $WorkComposer['widget_data']['worklist']['work']['chart_id'],
+															'tabindex'			=> 1,
 														]
 													]
 					])
@@ -33,7 +34,7 @@
 					<label class="control-label">Calendar</label>
 				</div>	
 				<div class="col-md-10">
-					@include('widgets.calendar.select', [
+					@include('widgets.organisation.calendar.select', [
 						'widget_options'		=> 	[
 														'calendarlist'			=>
 														[
@@ -43,6 +44,7 @@
 															'page'				=> 1,
 															'per_page'			=> 100,
 															'calendar_id'		=> $WorkComposer['widget_data']['worklist']['work']['calendar_id'],
+															'tabindex'			=> 2,
 														]
 													]
 					])
@@ -53,7 +55,7 @@
 					<label class="control-label">Status</label>
 				</div>	
 				<div class="col-md-10">
-					{!!Form::input('text', 'status', $WorkComposer['widget_data']['worklist']['work']['status'], ['class' => 'form-control'])!!}
+					{!!Form::input('text', 'status', $WorkComposer['widget_data']['worklist']['work']['status'], ['class' => 'form-control', 'tabindex' => 3])!!}
 				</div>
 			</div>
 			<div class="form-group">
@@ -61,13 +63,13 @@
 					<label class="control-label">Start</label>
 				</div>	
 				<div class="col-md-4">
-					{!!Form::input('text', 'start', $WorkComposer['widget_data']['worklist']['work']['start'], ['class' => 'form-control'])!!}
+					{!!Form::input('text', 'start', $WorkComposer['widget_data']['worklist']['work']['start'], ['class' => 'form-control date-mask', 'tabindex' => 4])!!}
 				</div>
 				<div class="col-md-1 col-md-offset-1">
 					<label class="control-label">End</label>
 				</div>	
 				<div class="col-md-4">
-					{!!Form::input('text', 'end', $WorkComposer['widget_data']['worklist']['work']['end'], ['class' => 'form-control'])!!}
+					{!!Form::input('text', 'end', $WorkComposer['widget_data']['worklist']['work']['end'], ['class' => 'form-control date-mask', 'tabindex' => 5])!!}
 				</div>
 			</div>
 			<div class="form-group">
@@ -75,7 +77,7 @@
 					<label class="control-label">Alasan Berhenti</label>
 				</div>	
 				<div class="col-md-10">
-					{!!Form::textarea('reason_end_job', $WorkComposer['widget_data']['worklist']['work']['reason_end_job'], ['class' => 'form-control'])!!}
+					{!!Form::textarea('reason_end_job', $WorkComposer['widget_data']['worklist']['work']['reason_end_job'], ['class' => 'form-control', 'tabindex' => 6])!!}
 				</div>
 			</div>
 			<div class="form-group">
--- resources/views/widgets/organisation/person/work/table.blade.php
+++ resources/views/widgets/organisation/person/work/table.blade.php
@@ -8,23 +8,37 @@
 	@section('widget_title')
 		<h1> {!! $widget_title or 'Karir' !!} </h1>
 		<small>Total data {{$WorkComposer['widget_data']['worklist']['work-pagination']->total()}}</small>
+
+		@if(isset($WorkComposer['widget_data']['worklist']['active_filter']) && !is_null($WorkComposer['widget_data']['worklist']['active_filter']))
+			 <div class="clearfix">&nbsp;</div>
+			@foreach($WorkComposer['widget_data']['worklist']['active_filter'] as $key => $value)
+				<span class="active-filter">{{$value}}</span>
+			@endforeach
+		@endif
 	@overwrite
 
 	@section('widget_body')
+		<a href="{{ $WorkComposer['widget_data']['worklist']['route_create'] }}" class="btn btn-primary">Tambah Data</a>
 		@if(isset($WorkComposer['widget_data']['worklist']['work']))
+			<div class="clearfix">&nbsp;</div>
 			<div class="table-responsive">
 				<table class="table">
 					<thead>
 						<tr class="row">
+							<th class="col-sm-1">No</th>
 							<th class="col-sm-3">Perusahaan</th>
 							<th class="col-sm-3">Jabatan (Status)</th>
-							<th class="col-sm-4" colspan="2">Lama Bekerja</th>
+							<th class="col-sm-3" colspan="2">Lama Bekerja</th>
 							<th class="col-sm-2">&nbsp;</th>
 						</tr>
 					</thead>
-					@foreach($WorkComposer['widget_data']['worklist']['work'] as $key => $value)
-						<tbody>
+					<?php $i = $WorkComposer['widget_data']['worklist']['work-display']['from'];?>
+					<tbody>
+						@foreach($WorkComposer['widget_data']['worklist']['work'] as $key => $value)
 							<tr class="row">
+								<td class="col-sm-1">
+									{{$i}}
+								</td>
 								@if($value['chart'])
 									<td class="col-sm-3">
 										{{$value['chart']['branch']['organisation']['name']}} Cabang {{$value['chart']['branch']['name']}}
@@ -40,7 +54,7 @@
 										{{$value['position']}} ({{$value['status']}})
 									</td>
 								@endif
-								<td class="col-sm-4" colspan="2">
+								<td class="col-sm-3" colspan="2">
 									@date_indo($value['start']) - 
 									@if(is_null($value['end']))
 										Sekarang
@@ -50,12 +64,13 @@
 								</td>
 								<td class="text-right col-sm-2">
 									<a href="javascript:;" class="btn btn-default" data-toggle="modal" data-target="#delete" data-delete-action="{{ route('hr.person.works.delete', [$value['id'], 'org_id' => $data['id'], 'person_id' => $person['id']]) }}"><i class="fa fa-trash"></i></a>
-									<a @if($value['chart']) href="{{route('hr.person.works.edit', [$value['id'], 'org_id' => $data['id'], 'person_id' => $person['id']])}}" @else href="{{route('hr.person.works.edit', [$value['id'], 'org_id' => $data['id'], 'person_id' => $person['id'], 'prev' => true])}}" @endif class="btn btn-default"><i class="fa fa-pencil"></i></a>
-									<a href="{{route('hr.person.works.show', [$value['id'], 'org_id' => $data['id'], 'person_id' => $person['id']])}}" class="btn btn-default"><i class="fa fa-eye"></i></a>
+									<a @if($value['chart']) href="{{route('hr.person.works.edit', [$value['id'], 'org_id' => $data['id'], 'person_id' => $person['id'], 'prev' => false])}}" @else href="{{route('hr.person.works.edit', [$value['id'], 'org_id' => $data['id'], 'person_id' => $person['id'], 'prev' => true])}}" @endif class="btn btn-default"><i class="fa fa-pencil"></i></a>
+									<!-- <a href="{{route('hr.person.works.show', [$value['id'], 'org_id' => $data['id'], 'person_id' => $person['id']])}}" class="btn btn-default"><i class="fa fa-eye"></i></a> -->
 								</td>
 							</tr>
-						</tbody>
-					@endforeach
+							<?php $i++;?>
+						@endforeach
+					</tbody>
 				</table>
 			</div>
 
