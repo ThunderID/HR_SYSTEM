fd3d1e6be25ce6af43e3c27669541ef16bcf840e
chelsymooy <chelsymooy1108@gmail.com>
2015-06-29 11:08:42 +0700

    Move everything to hr system as point

--- resources/views/pages/login/password.blade.php
+++ resources/views/pages/login/password.blade.php
@@ -1,4 +1,32 @@
-@section('area')
+@section('nav_topbar')
+	@include('widgets.common.nav_topbar', 
+	[
+		'breadcrumb' 	=> 	[
+								['name' => 'Ubah Password', 'route' => route('hr.password.get') ], 
+							]
+	])
+@stop
+
+@section('nav_sidebar')
+	@include('widgets.common.nav_sidebar', [
+		'widget_template'		=> 'plain_no_title',
+		'widget_title'			=> 'Structure',		
+		'widget_title_class'	=> 'text-uppercase ml-10 mt-20',
+		'widget_body_class'		=> '',
+		'widget_options'	=> [
+									'sidebar'				=>
+									[
+										'search'			=> ['id' => Session::get('user.organisationids')],
+										'sort'				=> [],
+										'page'				=> 1,
+										'per_page'			=> 100,
+										'active_dashboard'	=> 'yes'
+									]
+								]
+	])
+@overwrite
+
+@section('content_body')
 	@include('widgets.form.form_password', [
 		'widget_template'	=> 'panel',
 		'widget_options'	=> [
@@ -10,3 +38,9 @@
 								]
 	])		
 @stop
+
+@section('content_filter')
+@overwrite
+
+@section('content_footer')
+@overwrite
--- resources/views/widgets/common/nav_topbar.blade.php
+++ resources/views/widgets/common/nav_topbar.blade.php
@@ -23,7 +23,7 @@
 		</div>
 		 
 		<ul class="nav navbar-top-links navbar-right">
-			@if(Session::has('allow.filter') && Session::get('allow.filter')==true)
+			@if((Session::has('allow.filter') && Session::get('allow.filter')==true ) && isset($filter))
 				<li>
 					<a href="javascript:;" class="open-filter"><i class="fa fa-search"></i></a>
 				</li>
@@ -33,7 +33,7 @@
 					{!! HTML::image(Session::get('user.avatar'), '', array( 'width' => 32, 'height' => 32, 'class' => 'img-rounded' )) !!} {{Session::get('user.name')}} &nbsp;&nbsp; <i class="fa fa-caret-down"></i>
 				</a>
 				<ul class="dropdown-menu dropdown-user">					
-					<li><a href="{{route('hr.password.get')}}"><i class="fa fa-gear fa-fw"></i> Ganti Password</a></li>					
+					<li><a href="{{route('hr.password.get')}}"><i class="fa fa-gear fa-fw"></i> Ubah Password</a></li>					
 					<li><a href="{{route('hr.logout.get')}}"><i class="fa fa-sign-out fa-fw"></i> Logout</a></li>
 				</ul>	            
 			</li>	        
