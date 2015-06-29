44aa0b8676391efbc68fe934cd4daf53bcff5fef
chelsymooy <chelsymooy1108@gmail.com>
2015-06-29 09:31:05 +0700

    repatch idle

--- app/Http/Controllers/Organisation/IdleController.php
+++ app/Http/Controllers/Organisation/IdleController.php
@@ -40,11 +40,92 @@ class IdleController extends BaseController
 			App::abort(404);
 		}
 
+		$filter 									= [];
+
+		if(Input::has('q'))
+		{
+			$filter['search']['ondate']				= Input::get('q');
+			$filter['active']['q']					= 'Cari Tanggal "'.Input::get('q').'"';
+		}
+		if(Input::has('filter'))
+		{
+			$dirty_filter 							= Input::get('key');
+			$dirty_filter_value 					= Input::get('value');
+
+			foreach ($dirty_filter as $key => $value) 
+			{
+				if (str_is('search_*', strtolower($value)))
+				{
+					$filter_search 						= str_replace('search_', '', $value);
+					$filter['search'][$filter_search]	= $dirty_filter_value[$key];
+					$filter['active'][$filter_search]	= $dirty_filter_value[$key];
+					switch (strtolower($filter_search)) 
+					{
+						case 'ondate':
+							$active = 'Cari Tanggal ';
+							break;
+
+						default:
+							$active = 'Cari Tanggal ';
+							break;
+					}
+
+					switch (strtolower($dirty_filter_value[$key])) 
+					{
+						case 'asc':
+							$active = $active.'"'.$dirty_filter_value[$key].'"';
+							break;
+						
+						default:
+							$active = $active.'"'.$dirty_filter_value[$key].'"';
+							break;
+					}
+
+					$filter['active'][$filter_search]	= $active;
+
+				}
+				if (str_is('sort_*', strtolower($value)))
+				{
+					$filter_sort 						= str_replace('sort_', '', $value);
+					$filter['sort'][$filter_sort]		= $dirty_filter_value[$key];
+					switch (strtolower($filter_sort)) 
+					{
+						case 'start':
+							$active = 'Urutkan Tanggal Aktif ';
+							break;
+						
+						default:
+							$active = 'Urutkan Tanggal Aktif ';
+							break;
+					}
+
+					switch (strtolower($dirty_filter_value[$key])) 
+					{
+						case 'asc':
+							$active = $active.' (Z-A)';
+							break;
+						
+						default:
+							$active = $active.' (A-Z)';
+							break;
+					}
+
+					$filter['active'][$filter_sort]		= $active;
+				}
+			}
+		}
+
 		$data 									= json_decode(json_encode($contents->data), true);
 		$this->layout->page 					= view('pages.organisation.idle.index');
 		$this->layout->page->controller_name 	= $this->controller_name;
 		$this->layout->page->data 				= $data;
-		$this->layout->page->filter 			= [['filter' => 'urutkan','filters' => ['urutkan asc', 'urutkan desc']]];
+		$this->layout->page->filter 			= 	[
+														['prefix' => 'sort', 'key' => 'start', 'value' => 'Urutkan Tanggal Aktif', 'values' => [['key' => 'asc', 'value' => 'A-Z'], ['key' => 'desc', 'value' => 'Z-A']]]
+													];
+														
+		$this->layout->page->filtered 			= $filter;
+		$this->layout->page->default_filter  	= ['org_id' => $data['id']];
+
 		$this->layout->page->route_back 		= route('hr.organisations.show', $org_id);
 
 		return $this->layout;
@@ -99,8 +180,17 @@ class IdleController extends BaseController
 			$org_id 							= Session::get('user.organisation');
 		}
 
-		$attributes 							= Input::only('start', 'idle_1', 'idle_2');
-		$attributes['start'] 					= date('Y-m-d', strtotime($attributes['start']));
+		if(!in_array($org_id, Session::get('user.organisationids')))
+		{
+			App::abort(404);
+		}
+
+		$attributes 							= Input::only('idle_1', 'idle_2');
+		$attributes['created_by']				= Session::get('loggedUser');
+		if(Input::has('start'))
+		{
+			$attributes['start'] 				= date('Y-m-d', strtotime(Input::get('start')));
+		}
 
 		$errors 								= new MessageBag();
 
@@ -108,7 +198,7 @@ class IdleController extends BaseController
 
 		$content 								= $this->dispatch(new Saving(new SettingIdle, $attributes, $id, new Organisation, $org_id));
 		$is_success 							= json_decode($content);
-		
+
 		if(!$is_success->meta->success)
 		{
 			foreach ($is_success->meta->errors as $key => $value) 
