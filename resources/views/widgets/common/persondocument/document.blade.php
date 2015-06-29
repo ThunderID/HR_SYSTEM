ad7e2bbe7a7eac275aca89ffa8943416be03e3f5
chelsymooy <chelsymooy1108@gmail.com>
2015-06-29 08:44:25 +0700

    Fixed #23

---
+++ resources/views/widgets/common/persondocument/document.blade.php
@@ -0,0 +1,33 @@
+@extends('widget_templates.'.($widget_template ? $widget_template : 'plain'))
+
+@if ((isset($widget_errors) && !$widget_errors->count() || !isset($widget_errors)))
+	@section('widget_title')
+		<h1> {!! 'Dokumen '. $document['name'] .' "'.$person['name'].'"' !!} </h1>
+	@overwrite
+
+	@section('widget_body')
+		@if(isset($template) && !is_null($template) && $template!='')
+			{!!$template!!}
+		@else
+			<div class="clearfix">&nbsp;</div>
+			@foreach($persondocument['details'] as $key => $value)
+				<div class="row">
+					<div class="col-sm-3">
+						{!!ucwords($value['template']['field'])!!}
+					</div>
+					<div class="col-sm-9">
+						{!!($value['text'] ? ucwords($value['text']) : ucwords($value['numeric']))!!}
+					</div>
+				</div>
+				<div class="clearfix">&nbsp;</div>
+			@endforeach
+			<div class="clearfix">&nbsp;</div>
+		@endif
+	@overwrite	
+@else
+	@section('widget_title')
+	@overwrite
+
+	@section('widget_body')
+	@overwrite
+@endif
