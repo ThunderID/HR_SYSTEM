dda4117c43f3298f1d1d9aa0571733ceb74c8829
budi_p <budi-purnomo@outlook.com>
2015-06-30 18:23:51 +0700

    fix absence legend

---
+++ resources/views/widgets/common/absenceLegend.blade.php
@@ -0,0 +1,86 @@
+</br>
+<h2>Legend</h2>
+<div class="row">
+	<div class="col-md-4">
+		<div class="col-md-2">
+			<h4>SS</h4>
+		</div>
+		<div class="col-md-10">
+			<p>Sakit jangka pendek (Short Tern Sickness Absence)</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class="col-md-2">
+			<h4>SL</h4>
+		</div>
+		<div class="col-md-10">
+			<p>Sakit berkepanjangan (Long Tern Sickness Absence)</p>
+		</div>
+	</div>		
+	<div class="col-md-4">
+		<div class="col-md-2">
+			<h4>HT</h4>
+		</div>
+		<div class="col-md-10">
+			<p>Kehadiran cacat namun disertai dengan ijin datang terlambat</p>
+		</div>
+	</div>		
+</div>
+<div class="row">
+	<div class="col-md-4">
+		<div class="col-md-2">
+			<h4>HP</h4>
+		</div>
+		<div class="col-md-10">
+			<p>Kehadiran cacat namun disertai dengan ijin pulang cepat</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class="col-md-2">
+			<h4>HD</h4>
+		</div>
+		<div class="col-md-10">
+			<p>Kehadiran cacat namun disertai dengan ijin dinas</p>
+		</div>
+	</div>		
+	<div class="col-md-4">
+		<div class="col-md-2">
+			<h4>DN</h4>
+		</div>
+		<div class="col-md-10">
+			<p style="margin-top:9px;">Keperluan dinas</p>
+		</div>
+	</div>		
+</div>
+<div class="row">
+	<div class="col-md-4">
+		<div class="col-md-2">
+			<h4>CN</h4>
+		</div>
+		<div class="col-md-10">
+			<p style="margin-top:9px;">Cuti untuk keperluan pribadi</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class="col-md-2">
+			<h4>CB</h4>
+		</div>
+		<div class="col-md-10">
+			<p style="margin-top:9px;">Cuti bersama</p>
+		</div>
+	</div>		
+	<div class="col-md-4">
+		<div class="col-md-2">
+			<h4>CI</h4>
+		</div>
+		<div class="col-md-10">
+			<p style="margin-top:9px;">Cuti istimewa</p>
+		</div>
+	</div>		
+</div>	
+</br>
+<div class="row">
+	<div class="col-md-12 text-right">
+		<small>* Sesuai dengan ketentuan (Policy & SOP) yang berlaku.</small>
+	</div>	
+</div>	
