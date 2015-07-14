616639566d1f223cb0f58090ba8d1fbef3221958
uchuy77 <agil.mahendra@gmail.com>
2015-07-14 13:46:55 +0700

    add plugin sticky affix header

---
+++ resources/views/plugins/stickytableheader.blade.php
@@ -0,0 +1,4 @@
+{!! HTML::style('plugins/stickytableheader/component.css') !!}
+{!! HTML::style('plugins/stickytableheader/normalize.css') !!}
+{!! HTML::script('plugins/stickytableheader/jquery.ba-throttle-debounce.min.js') !!}
+{!! HTML::script('plugins/stickytableheader/jquery.stickyheader.js') !!}
