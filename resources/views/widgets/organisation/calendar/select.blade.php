b6c0d2664086be32058a904ffe6548243ab783f4
uchuy77 <agil.mahendra@gmail.com>
2015-08-01 12:21:18 +0700

    modified select add null value

--- resources/views/widgets/organisation/calendar/select.blade.php
+++ resources/views/widgets/organisation/calendar/select.blade.php
@@ -1,4 +1,5 @@
 <select name="calendar_id" class="form-control select2 {{ isset($CalendarComposer['widget_data']['calendarlist']['class_name']) ? $CalendarComposer['widget_data']['calendarlist']['class_name'] : '' }}" tabindex="{{ $CalendarComposer['widget_data']['calendarlist']['tabindex'] }}">
+	<option value="0">&nbsp;</option>
 	@foreach($CalendarComposer['widget_data']['calendarlist']['calendar'] as $key => $value)
 		<option value="{{$value['id']}}" @if($value['id']==$CalendarComposer['widget_data']['calendarlist']['calendar_id']) selected @endif>{{$value['name']}}</option>
 	@endforeach
