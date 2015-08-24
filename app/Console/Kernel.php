a20e92a6bbdda04ca18c0f7027b9de507b217e88
chelsymooy <chelsymooy1108@gmail.com>
2015-08-24 21:30:32 +0700

    scheduled queue in time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,11 +12,17 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected $commands = [
 		'App\Console\Commands\Inspire',
+
+		'App\Console\Commands\WorkleaveQueueCommand',
+		'App\Console\Commands\AbsenceQueueCommand',
+		'App\Console\Commands\AttendanceQueueCommand',
+		'App\Console\Commands\SanctionQueueCommand',
+		'App\Console\Commands\HRQueueCommand',
+
 		'App\Console\Commands\LogAbsenceCommand',
 		'App\Console\Commands\FirstLockCommand',
 		'App\Console\Commands\SanctionCommand',
-		'App\Console\Commands\WorkleaveQueueCommand',
-		'App\Console\Commands\HRQueueCommand',
+		
 		'App\Console\Commands\ScheduleBatchCommand',
 		'App\Console\Commands\PersonScheduleBatchCommand',
 		'App\Console\Commands\PersonWorkleaveBatchCommand',
@@ -33,11 +39,11 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected function schedule(Schedule $schedule)
 	{
-		$schedule->command('hr:absence LogAbsenceCommand')
+		$schedule->command('hr:absencequeue AbsenceQueueCommand')
 				 ->daily();
-		$schedule->command('hr:firstlock FirstLockCommand')
+		$schedule->command('hr:attendancequeue AttendanceQueueCommand')
 				 ->cron('0 0 */21 * * *');
-		$schedule->command('hr:firstlock SanctionCommand')
+		$schedule->command('hr:sanctionqueue SanctionQueueCommand')
 				 ->daily();
 		 $schedule->command('hr:workleavequeue WorkleaveQueueCommand')
 				 ->monthly();
