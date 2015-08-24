ea1df838425738b1d647d05c29932417668c57c6
uchuy77 <agil.mahendra@gmail.com>
2015-08-24 11:58:49 +0700

    modified page create & index policy

--- resources/views/pages/organisation/policy/index.blade.php
+++ resources/views/pages/organisation/policy/index.blade.php
@@ -19,7 +19,7 @@
 											'sort'				=> [],
 											'page'				=> 1,
 											'per_page'			=> 100,
-											'active_idle'		=> 'yes',
+											'active_policy'		=> 'yes',
 											'pengaturan'		=> 'yes'
 										]
 									]
