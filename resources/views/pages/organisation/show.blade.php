847be02a7dfb31c2a7cfeb5d16ed80cdcf513618
uchuy77 <agil.mahendra@gmail.com>
2015-08-25 10:36:13 +0700

    dashboard revert

--- resources/views/pages/organisation/show.blade.php
+++ resources/views/pages/organisation/show.blade.php
@@ -28,18 +28,92 @@
 @overwrite
 
 @section('content_body')
-	@include('widgets.organisation.dashboard.widget', [
-		'widget_template'	=> 'panel_no_title',
-		'widget_options'	=> [
-									'dashboard'			=> 
-									[
-										'search'		=> '',
-										'sort'			=> [],
-										'page'			=> 1,
-										'per_page'		=> 100
-									]
-								]
-	])
+	<div class="row mb-25">
+		<div class="col-sm-6 col-md-6">
+			@include('widgets.organisation.person.stat.total_employee', [
+				'widget_template'		=> 'plain',
+				'widget_title'			=> 'Total Karyawan '.$data['name'],
+				'widget_options'		=> 	[
+												'personlist'		=>
+												[
+													'title'				=> 'Total Karyawan "'.$data['name'].'"',
+													'organisation_id'	=> $data['id'],
+													'search'			=> ['chartnotadmin' => true],
+													'sort'				=> [],
+													'page'				=> 1,
+													'per_page'			=> 100,
+												]
+											]
+			])
+		</div>
+		<div class="col-sm-6 col-md-6">
+			@include('widgets.organisation.branch.stat.total_branch', [
+				'widget_template'		=> 'plain',
+				'widget_title'			=> 'Total Cabang '.$data['name'],
+				'widget_options'		=> 	[
+												'branchlist'		=>
+												[
+													'organisation_id'	=> $data['id'],
+													'search'			=> [],
+													'sort'				=> [],
+													'page'				=> 1,
+													'per_page'			=> 100,
+												]
+											]
+			])
+		</div>
+		<div class="col-sm-6 col-md-6">
+			@include('widgets.organisation.document.stat.total_document', [
+				'widget_template'		=> 'plain',
+				'widget_title'			=> 'Total Dokumen '.$data['name'],
+				'widget_options'		=> 	[
+												'documentlist'		=>
+												[
+													'organisation_id'	=> $data['id'],
+													'search'			=> [],
+													'sort'				=> [],
+													'page'				=> 1,
+													'per_page'			=> 100,
+												]
+											]
+			])
+		</div>
+		<div class="col-sm-6 col-md-6">
+			@include('widgets.organisation.person.stat.average_loss_rate', [
+				'widget_template'		=> 'plain',
+				'widget_title'			=> 'Average Loss Rate '.$data['name'],
+				'widget_options'		=> 	[
+												'lossratelist'		=>
+												[
+													'organisation_id'	=> $data['id'],
+													'search'			=> ['globalattendance' => ['organisationid' => $data['id'], 'on' => [$start, $end]]],
+													'sort'				=> [],
+													'page'				=> 1,
+													'per_page'			=> 40,
+												]
+											]
+			])
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-12">
+			@include('widgets.organisation.person.table', [
+				'widget_template'		=> 'panel',
+				'widget_title'			=> '<h4>Absen Karyawan "'.$data['name'].'" ('.date('d-m-Y', strtotime('- 1 day')).')</h4>',
+				'widget_options'		=> 	[
+												'personlist'		=>
+												[
+													'organisation_id'	=> $data['id'],
+													'search'			=> ['fullschedule' => date('Y-m-d', strtotime('- 1 day')), 'withattributes' => ['works.branch']],
+													'sort'				=> [],
+													'page'				=> 1,
+													'per_page'			=> 12,
+													'route_create'		=> route('hr.persons.create', ['org_id' => $data['id']])
+												]
+											]
+			])
+		</div>
+	</div>
 @stop
 
 @section('content_filter')
