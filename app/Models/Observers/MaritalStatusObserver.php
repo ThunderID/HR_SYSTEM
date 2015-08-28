38da1dc7951c092ba3ed894c1ea972ce414bb205
chelsymooy <chelsymooy1108@gmail.com>
2015-08-28 12:26:11 +0700

    add event record log on data

--- app/Models/Observers/MaritalStatusObserver.php
+++ app/Models/Observers/MaritalStatusObserver.php
@@ -1,6 +1,7 @@
 <?php namespace App\Models\Observers;
 
-use \Validator;
+use \Validator, Event;
+use App\Events\CreateRecordOnTable;
 
 /* ----------------------------------------------------------------------
  * Event:
@@ -26,8 +27,38 @@ class MaritalStatusObserver
 		}
 	}
 
-	public function deleting($model)
+	public function created($model)
 	{
-		//
+		$attributes['record_log_id'] 		= $model->id;
+		$attributes['record_log_type'] 		= get_class($model);
+		$attributes['name'] 				= 'Menambah Status Pernikahan';
+		$attributes['notes'] 				= 'Menambah Status Pernikahan'.' pada '.date('d-m-Y');
+		$attributes['action'] 				= 'delete';
+
+		Event::fire(new CreateRecordOnTable($attributes));
+	}
+
+	public function updated($model)
+	{
+		$attributes['record_log_id'] 		= $model->id;
+		$attributes['record_log_type'] 		= get_class($model);
+		$attributes['name'] 				= 'Mengubah Status Pernikahan ';
+		$attributes['notes'] 				= 'Mengubah Status Pernikahan '.' pada '.date('d-m-Y');
+		$attributes['old_attribute'] 		= json_encode($model->getOriginal());
+		$attributes['new_attribute'] 		= json_encode($model->getAttributes());
+		$attributes['action'] 				= 'save';
+
+		Event::fire(new CreateRecordOnTable($attributes));
+	}
+
+	public function deleted($model)
+	{
+		$attributes['record_log_id'] 		= $model->id;
+		$attributes['record_log_type'] 		= get_class($model);
+		$attributes['name'] 				= 'Menghapus Status Pernikahan';
+		$attributes['notes'] 				= 'Menghapus Status Pernikahan'.' pada '.date('d-m-Y');
+		$attributes['action'] 				= 'restore';
+
+		Event::fire(new CreateRecordOnTable($attributes));
 	}
 }
