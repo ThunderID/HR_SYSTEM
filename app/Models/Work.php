931dcc208c112cdb99adeb7a7aa8a452b25b9783
chelsymooy <chelsymooy1108@gmail.com>
2015-09-01 10:34:39 +0700

    back track person work before add batch workleave

--- app/Models/Work.php
+++ app/Models/Work.php
@@ -164,6 +164,11 @@ class Work extends BaseModel {
 		return $query->whereHas('calendar', function($q){$q;});
 	}
 
+	public function scopeStartBefore($query, $variable)
+	{
+		return $query->where('start', '<=', date('Y-m-d', strtotime($variable)));
+	}
+
 	public function scopeGroupPerson($query, $variable)
 	{
 		return $query->groupBy('person_id');
