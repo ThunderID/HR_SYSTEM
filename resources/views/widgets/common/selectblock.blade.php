47bd817e99883a05a142baf1e398b90e8e176925
uchuy77 <agil.mahendra@gmail.com>
2015-09-03 11:59:09 +0700

    add acl in menu import csv document, person document, person

--- resources/views/widgets/common/selectblock.blade.php
+++ resources/views/widgets/common/selectblock.blade.php
@@ -2,6 +2,9 @@
 	<div class="col-sm-12 text-left">
 		<a href="{{ $widget_options['url_old'] }}" class="btn btn-primary">{{$widget_options['caption_old']}}</a>
 		<a href="{{ $widget_options['url_new'] }}" class="btn btn-primary">{{$widget_options['caption_new']}}</a>
-		<a href="javascript:;" class="btn btn-primary" data-toggle="modal" data-target="#import_csv_work_person" data-action="{{ route('hr.person.works.store') }}" data-org_id="{{ $data['id'] }}">Import CSV</a>
+
+		@if (Route::is('hr.person.works.index')&((int)Session::get('user.menuid') <= 3))
+			<a href="javascript:;" class="btn btn-primary" data-toggle="modal" data-target="#import_csv_work_person" data-action="{{ route('hr.person.works.store') }}" data-org_id="{{ $data['id'] }}">Import CSV</a>
+		@endif
 	</div>
 </div>
