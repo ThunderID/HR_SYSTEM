4c2d8c9550a07059b264404321b1c8b5379d8a7d
chelsymooy <chelsymooy1108@gmail.com>
2015-09-07 12:42:44 +0700

    working on fingerprint route and function (renew) To do : test via web and fp's device

---
+++ app/Http/Controllers/FP/FingerController.php
@@ -0,0 +1,285 @@
+<?php namespace App\Http\Controllers\FP;
+
+use Input, Session, App, Paginator, Redirect, DB, Config, Validator, Image, Response;
+use App\Http\Controllers\BaseController;
+use Illuminate\Support\MessageBag;
+use App\Console\Commands\Saving;
+use App\Console\Commands\Getting;
+use App\Console\Commands\Deleting;
+use App\Console\Commands\Checking;
+use App\Models\Organisation;
+use App\Models\Log;
+use App\Models\Person;
+use App\Models\ErrorLog;
+use App\Models\Branch;
+use App\Models\Api;
+
+class FingerController extends BaseController 
+{
+
+	protected $controller_name 					= 'data';
+
+	public function store()
+	{		
+		$attributes 							= Input::only('application', 'data');
+
+		//cek apa ada aplication
+		if(!$attributes['application'])
+		{
+			return Response::json('101', 200);
+		}
+
+		if(!isset($attributes['application']['api']['client']) || !isset($attributes['application']['api']['secret']) || !isset($attributes['application']['api']['station_id']))
+		{
+			return Response::json('102', 200);
+		}	
+
+		if(!$attributes['application']['api']['username'])
+		{
+			return Response::json('106', 200);
+		}
+
+		//cek API key & secret
+		$results 								= $this->dispatch(new Getting(new API, ['client' => $attributes['application']['api']['client'], 'secret' => $attributes['application']['api']['secret'], 'workstationaddress' => $attributes['application']['api']['station_id'], 'withattributes' => ['branch']], [], 1, 1));
+		
+		$content 								= json_decode($results);
+		if(!$content->meta->success)
+		{
+			return Response::json('402', 200);
+		}
+
+		if(isset($attributes['application']['api']['version']) && strtolower($attributes['application']['api']['version'])!=strtolower($content->data->tr_version))
+		{
+			$apiattributes 						= json_decode(json_encode($content->data), true);
+			$apiattributes['tr_version']		= strtolower($attributes['application']['api']['version']);
+
+			$content_2 							= $this->dispatch(new Saving(new API, $apiattributes, $apiattributes['id'], new Branch, $apiattributes['branch_id']));
+			$is_success 						= json_decode($content_2);
+			
+			if(!$is_success->meta->success)
+			{
+				return Response::json('301', 200);
+			}
+		}
+
+		$organisationid 						= $content->data->branch->organisation_id;
+
+		//cek apa ada data data
+		if(!$attributes['data'])
+		{
+			return Response::json('103', 200);
+		}
+
+		//cek apa data bisa disimpan
+		DB::beginTransaction();
+		if(isset($attributes['data']))
+		{
+			$attributes['data']				= (array)$attributes['data'];
+			$data 							= $this->dispatch(new Getting(new Person, ['username' => $attributes['application']['api']['username'], 'withattributes' => ['finger']], [] , 1, 1), false);
+			$person 						= json_decode($data);
+
+			if(!$person->meta->success)
+			{
+				$data['email']				= $value[0];
+				$data['message']			= json_encode($person->meta->errors);
+				$saved_error_data 			= $this->dispatch(new Saving(new ErrorLog, $data, null, new Organisation, $organisationid));
+			}
+			elseif(!$person->data->finger)
+			{
+				$data['email']				= $person->data->username;
+				$data['message']			= json_encode(['Tidak ada data finger']);
+				$saved_error_data 			= $this->dispatch(new Saving(new ErrorLog, $data, null, new Organisation, $organisationid));
+			}
+			else
+			{
+				$data 						= [];
+				foreach ($attributes['data'] as $key => $value) 
+				{
+					unset($activefinger)
+					switch(strtolower($key))
+					{
+						case 'jari1' :
+							$activefinger 		= 'left_thumb';
+							break;
+						case 'jari2' :
+							$activefinger 		= 'left_index_finger';
+							break;
+						case 'jari3' :
+							$activefinger 		= 'left_middle_finger';
+							break;
+						case 'jari4' :
+							$activefinger 		= 'left_ring_finger';
+							break;
+						case 'jari5' :
+							$activefinger 		= 'left_little_finger';
+							break;
+						case 'jari6' :
+							$activefinger 		= 'right_thumb';
+							break;
+						case 'jari7' :
+							$activefinger 		= 'right_index_finger';
+							break;
+						case 'jari8' :
+							$activefinger 		= 'right_middle_finger';
+							break;
+						case 'jari9' :
+							$activefinger 		= 'right_ring_finger';
+							break;
+						case 'jari10' :
+							$activefinger 		= 'right_little_finger';
+							break;
+					}
+
+					if(isset($activefinger))
+					{
+						$data[$activefinger]  	= $value;
+					}
+				}
+
+				$saved_data 					= $this->dispatch(new Saving(new FingerPrint, $data, $person->data->finger->id, new Person, $person->data->id));
+				$is_success_2 					= json_decode($saved_data);
+				if(!$is_success_2->meta->success)
+				{
+					$data['email']				= $person->data->name;
+					$data['message']			= $is_success_2->meta->errors;
+					$saved_error_data 			= $this->dispatch(new Saving(new ErrorLog, $data, null, new Organisation, $organisationid));
+				}
+			}
+		}
+
+		DB::commit();
+		return Response::json('Sukses', 200);
+	}
+
+	public function destroy()
+	{		
+		$attributes 							= Input::only('application', 'data');
+
+		//cek apa ada aplication
+		if(!$attributes['application'])
+		{
+			return Response::json('101', 200);
+		}
+
+		if(!isset($attributes['application']['api']['client']) || !isset($attributes['application']['api']['secret']) || !isset($attributes['application']['api']['station_id']))
+		{
+			return Response::json('102', 200);
+		}	
+
+		if(!$attributes['application']['api']['username'])
+		{
+			return Response::json('106', 200);
+		}
+
+		//cek API key & secret
+		$results 								= $this->dispatch(new Getting(new API, ['client' => $attributes['application']['api']['client'], 'secret' => $attributes['application']['api']['secret'], 'workstationaddress' => $attributes['application']['api']['station_id'], 'withattributes' => ['branch']], [], 1, 1));
+		
+		$content 								= json_decode($results);
+		if(!$content->meta->success)
+		{
+			return Response::json('402', 200);
+		}
+
+		if(isset($attributes['application']['api']['version']) && strtolower($attributes['application']['api']['version'])!=strtolower($content->data->tr_version))
+		{
+			$apiattributes 						= json_decode(json_encode($content->data), true);
+			$apiattributes['tr_version']		= strtolower($attributes['application']['api']['version']);
+
+			$content_2 							= $this->dispatch(new Saving(new API, $apiattributes, $apiattributes['id'], new Branch, $apiattributes['branch_id']));
+			$is_success 						= json_decode($content_2);
+			
+			if(!$is_success->meta->success)
+			{
+				return Response::json('301', 200);
+			}
+		}
+
+		$organisationid 						= $content->data->branch->organisation_id;
+
+		//cek apa ada data data
+		if(!$attributes['data'])
+		{
+			return Response::json('103', 200);
+		}
+
+		//cek apa data bisa disimpan
+		DB::beginTransaction();
+		if(isset($attributes['data']))
+		{
+			$attributes['data']				= (array)$attributes['data'];
+			$data 							= $this->dispatch(new Getting(new Person, ['username' => $attributes['application']['api']['username'], 'withattributes' => ['finger']], [] , 1, 1), false);
+			$person 						= json_decode($data);
+
+			if(!$person->meta->success)
+			{
+				$data['email']				= $value[0];
+				$data['message']			= json_encode($person->meta->errors);
+				$saved_error_data 			= $this->dispatch(new Saving(new ErrorLog, $data, null, new Organisation, $organisationid));
+			}
+			elseif(!$person->data->finger)
+			{
+				$data['email']				= $person->data->username;
+				$data['message']			= json_encode(['Tidak ada data finger']);
+				$saved_error_data 			= $this->dispatch(new Saving(new ErrorLog, $data, null, new Organisation, $organisationid));
+			}
+			else
+			{
+				$data 						= [];
+				foreach ($attributes['data'] as $key => $value) 
+				{
+					unset($activefinger)
+					switch(strtolower($key))
+					{
+						case 'jari1' :
+							$activefinger 		= 'left_thumb';
+							break;
+						case 'jari2' :
+							$activefinger 		= 'left_index_finger';
+							break;
+						case 'jari3' :
+							$activefinger 		= 'left_middle_finger';
+							break;
+						case 'jari4' :
+							$activefinger 		= 'left_ring_finger';
+							break;
+						case 'jari5' :
+							$activefinger 		= 'left_little_finger';
+							break;
+						case 'jari6' :
+							$activefinger 		= 'right_thumb';
+							break;
+						case 'jari7' :
+							$activefinger 		= 'right_index_finger';
+							break;
+						case 'jari8' :
+							$activefinger 		= 'right_middle_finger';
+							break;
+						case 'jari9' :
+							$activefinger 		= 'right_ring_finger';
+							break;
+						case 'jari10' :
+							$activefinger 		= 'right_little_finger';
+							break;
+					}
+
+					if(isset($activefinger))
+					{
+						$data[$activefinger]  	= '';
+					}
+				}
+
+				$saved_data 					= $this->dispatch(new Saving(new FingerPrint, $data, $person->data->finger->id, new Person, $person->data->id));
+				$is_success_2 					= json_decode($saved_data);
+				if(!$is_success_2->meta->success)
+				{
+					$data['email']				= $person->data->name;
+					$data['message']			= $is_success_2->meta->errors;
+					$saved_error_data 			= $this->dispatch(new Saving(new ErrorLog, $data, null, new Organisation, $organisationid));
+				}
+			}
+		}
+		
+		DB::commit();
+		return Response::json('Sukses', 200);
+	}
+}
---
+++ app/Http/Controllers/FP/LogController.php
@@ -0,0 +1,114 @@
+<?php namespace App\Http\Controllers\FP;
+
+use Input, Session, App, Paginator, Redirect, DB, Config, Validator, Image, Response;
+use App\Http\Controllers\BaseController;
+use Illuminate\Support\MessageBag;
+use App\Console\Commands\Saving;
+use App\Console\Commands\Getting;
+use App\Console\Commands\Deleting;
+use App\Console\Commands\Checking;
+use App\Models\Organisation;
+use App\Models\Log;
+use App\Models\Person;
+use App\Models\ErrorLog;
+use App\Models\Branch;
+use App\Models\Api;
+
+class LogController extends BaseController 
+{
+
+	protected $controller_name 					= 'log';
+
+	public function store()
+	{		
+		$attributes 							= Input::only('application', 'log');
+
+		//cek apa ada aplication
+		if(!$attributes['application'])
+		{
+			return Response::json('101', 200);
+		}
+
+		if(!isset($attributes['application']['api']['client']) || !isset($attributes['application']['api']['secret']) || !isset($attributes['application']['api']['station_id']))
+		{
+			return Response::json('102', 200);
+		}	
+
+		if(!$attributes['application']['api']['station_name'])
+		{
+			return Response::json('105', 200);
+		}
+
+		//cek API key & secret
+		$results 								= $this->dispatch(new Getting(new API, ['client' => $attributes['application']['api']['client'], 'secret' => $attributes['application']['api']['secret'], 'workstationaddress' => $attributes['application']['api']['station_id'], 'withattributes' => ['branch']], [], 1, 1));
+		
+		$content 								= json_decode($results);
+		if(!$content->meta->success)
+		{
+			$filename                       	= storage_path().'/logs/appid.log';
+			$fh                             	= fopen($filename, 'a+'); 
+			$template 							= date('Y-m-d H:i:s : Log : ').json_encode($attributes['application']['api'])."\n";
+	        fwrite($fh, $template); 
+	        fclose($fh);
+
+			return Response::json('402', 200);
+		}
+
+		if(isset($attributes['application']['api']['version']) && strtolower($attributes['application']['api']['version'])!=strtolower($content->data->tr_version))
+		{
+			$apiattributes 						= json_decode(json_encode($content->data), true);
+			$apiattributes['tr_version']		= strtolower($attributes['application']['api']['version']);
+
+			$content_2 							= $this->dispatch(new Saving(new API, $apiattributes, $apiattributes['id'], new Branch, $apiattributes['branch_id']));
+			$is_success 						= json_decode($content_2);
+			
+			if(!$is_success->meta->success)
+			{
+				return Response::json('301', 200);
+			}
+		}
+
+		$organisationid 						= $content->data->branch->organisation_id;
+
+		//cek apa ada data log
+		if(!$attributes['log'])
+		{
+			return Response::json('103', 200);
+		}
+
+		//cek apa data bisa disimpan
+		DB::beginTransaction();
+		if(isset($attributes['log']))
+		{
+			$attributes['log']					= (array)$attributes['log'];
+			foreach ($attributes['log'] as $key => $value) 
+			{
+				$log['name']					= strtolower('fingerprint');
+				$log['on']						= date("Y-m-d H:i:s", strtotime($value[1]));
+				$log['pc']						= $attributes['application']['api']['station_name'];
+				$data 							= $this->dispatch(new Getting(new Person, ['username' => $value[0]], [] , 1, 1), false);
+				$person 						= json_decode($data);
+
+				if(!$person->meta->success)
+				{
+					$log['email']				= $value[0];
+					$log['message']				= json_encode($person->meta->errors);
+					$saved_error_log 			= $this->dispatch(new Saving(new ErrorLog, $log, null, new Organisation, $organisationid));
+				}
+				else
+				{
+					$saved_log 					= $this->dispatch(new Saving(new Log, $log, null, new Person, $person->data->id));
+					$is_success_2 				= json_decode($saved_log);
+					if(!$is_success_2->meta->success)
+					{
+						$log['email']			= $value[0];
+						$log['message']			= $is_success_2->meta->errors;
+						$saved_error_log 		= $this->dispatch(new Saving(new ErrorLog, $log, null, new Organisation, $organisationid));
+					}
+				}
+			}
+		}
+		DB::commit();
+		return Response::json('Sukses', 200);
+	}
+}
