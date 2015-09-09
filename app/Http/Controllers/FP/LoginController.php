6e425e285a50713c1db9bfeffdbdb85968c4269a
chelsymooy <chelsymooy1108@gmail.com>
2015-09-09 11:26:11 +0700

    add test route for fp

--- app/Http/Controllers/FP/LoginController.php
+++ app/Http/Controllers/FP/LoginController.php
@@ -15,9 +15,66 @@ use Auth, Input, Session, Redirect, Response, DateTimeZone, DateTime, DB, Log;
 
 class LoginController extends BaseController {
 
+	function testLogin()
+	{
+		$attributes 							= Input::only('application');
+
+		//cek apa ada aplication
+		if(!$attributes['application'])
+		{
+			return Response::json('101', 200);
+		}		
+
+		if(!isset($attributes['application']['api']['client']) || !isset($attributes['application']['api']['secret']) || !isset($attributes['application']['api']['version']) || !isset($attributes['application']['api']['station_id']))
+		{
+			return Response::json('102', 200);
+		}
+
+		//cek API key & secret
+		$results 								= $this->dispatch(new Getting(new API, ['client' => $attributes['application']['api']['client'], 'secret' => $attributes['application']['api']['secret'], 'workstationaddress' => $attributes['application']['api']['station_id'], 'withattributes' => ['branch']], [], 1, 1));
+		
+		$content 								= json_decode($results);
+		if(!$content->meta->success)
+		{
+			$results_2 							= $this->dispatch(new Getting(new API, ['client' => $attributes['application']['api']['client'], 'secret' => $attributes['application']['api']['secret'], 'withattributes' => ['branch']], [], 1, 1));
+		
+			$content_2 							= json_decode($results_2);
+			
+			if(!$content_2->meta->success)
+			{
+				$filename                       	= storage_path().'/logs/appid.log';
+				$fh                             	= fopen($filename, 'a+'); 
+				$template 							= date('Y-m-d H:i:s : Test : ').json_encode($attributes['application']['api'])."\n";
+		        fwrite($fh, $template); 
+		        fclose($fh);
+
+				return Response::json('401', 200);
+			}
+			else
+			{
+				return Response::json('201', 200);
+			}
+		}
+
+		if(strtolower($attributes['application']['api']['version'])!=strtolower($content->data->tr_version))
+		{
+			$apiattributes 						= json_decode(json_encode($content->data), true);
+			$apiattributes['tr_version']		= strtolower($attributes['application']['api']['version']);
+
+			$content 							= $this->dispatch(new Saving(new API, $apiattributes, $apiattributes['id'], new Branch, $apiattributes['branch_id']));
+			$is_success 						= json_decode($content);
+			
+			if(!$is_success->meta->success)
+			{
+				return Response::json('301', 200);
+			}
+		}
+
+		return Response::json('Sukses', 200);
+	}
+
 	function postLogin()
 	{
-
 		$attributes 							= Input::only('application');
 
 		//cek apa ada aplication
