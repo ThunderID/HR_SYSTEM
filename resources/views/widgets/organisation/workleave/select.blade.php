84253fe4b592f7342281e07053697bb3e5b926d2
uchuy77 <agil.mahendra@gmail.com>
2015-09-23 16:15:28 +0700

    modified select chart workleave

--- resources/views/widgets/organisation/workleave/select.blade.php
+++ resources/views/widgets/organisation/workleave/select.blade.php
@@ -1,4 +1,4 @@
-<select name="workleave_id" tabindex = {{$WorkleaveComposer['widget_data']['workleavelist']['tabindex']}} class="form-control">
+<select name="workleave_id" tabindex = {{$WorkleaveComposer['widget_data']['workleavelist']['tabindex']}} class="form-control select2">
 	@foreach($WorkleaveComposer['widget_data']['workleavelist']['workleave'] as $key => $value)
 		<option value="{{$value['id']}}" @if($value['id']==$WorkleaveComposer['widget_data']['workleavelist']['workleave_id']) selected @endif>{{$value['name']}}</option>
 	@endforeach
