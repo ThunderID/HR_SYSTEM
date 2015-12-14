87102e7010dd76abf38f925183c84235b1db1778
chelsymooy <chelsymooy1108@gmail.com>
2015-12-14 14:04:12 +0700

    bug on displaying quota

--- resources/views/widgets/organisation/person/workleave/left_quota.blade.php
+++ resources/views/widgets/organisation/person/workleave/left_quota.blade.php
@@ -4,7 +4,7 @@
 		@if(isset($PersonComposer['widget_data']['personlist']['person']))
 			<div class="alert alert-callout alert-info no-margin">
 				<strong class="pull-right text-info text-lg"><i class="fa fa-bed fa-2x"></i></strong>
-				<h5>{{($PersonComposer['widget_data']['personlist']['person']['quotas'] + $PersonComposer['widget_data']['personlist']['person']['plus_quotas'] - $PersonComposer['widget_data']['personlist']['person']['minus_quotas'] >= 0)}}
+				<h5>{{($PersonComposer['widget_data']['personlist']['person']['quotas'] + $PersonComposer['widget_data']['personlist']['person']['plus_quotas'] - $PersonComposer['widget_data']['personlist']['person']['minus_quotas'])}}
 				</h5>
 				<span class="opacity-50">{!! $widget_title  or 'Sisa Cuti "'.$data['name'].'" Bulan Ini' !!} </span>					
 			</div>
