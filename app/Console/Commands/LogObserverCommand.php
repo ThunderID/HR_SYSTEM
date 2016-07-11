e76aee793da2dd3c758de9e663566bd931cfda52
chelsymooy <chelsymooy1108@gmail.com>
2016-07-11 11:02:33 +0700

    super typo ^^'

--- app/Console/Commands/LogObserverCommand.php
+++ app/Console/Commands/LogObserverCommand.php
@@ -397,7 +397,7 @@ class LogObserverCommand extends Command {
 
 				foreach ($idle as $key => $value) 
 				{
-					if(date('Y-m-d', strtotime($value['on'])) != date('Y-m-d', strtotime($value['last_input_time'])) && date('Y-m-d', strtotime($parameters['on'])) == date('Y-m-d', strtotime($value['on'])))
+					if(date('Y-m-d', strtotime($value['on'])) != date('Y-m-d', strtotime($value['last_input_time'])) && date('Y-m-d', strtotime($parameters['on'])) == date('Y-m-d', strtotime($value['last_input_time'])))
 					{
 						$start 			= date('H:i:s', strtotime($value['on']));
 					}
